71daf811d1d7a8d633ad0749dfa3d0584d7f0c57
valecasas <112732745+valecasas@users.noreply.github.com>
2022-11-11 19:31:27 -0500

    Add files via upload

---
+++ Hilos/src/hilos/Multi.java
@@ -0,0 +1,34 @@
+
+package hilos;
+
+/* Clase que crea tres instancias de la clase Hilo y los ejecuta
+
+* para representar el comportamiento de los Hilos en JAVA.
+
+*/
+
+public class Multi {
+
+public static void main( String args[] ) {
+
+Hilos t1,t2,t3;
+
+// Creación de los threads con parámetro tiempo aleatorio
+
+t1 = new Hilos( "Hilo 1",(int)(Math.random()*2000));
+
+t2 = new Hilos( "Hilo 2",(int)(Math.random()*2000));
+
+t3 = new Hilos( "Hilo 3",(int)(Math.random()*2000));
+
+// Arrancamos los threads con el método start
+
+t1.start();
+
+t2.start();
+
+t3.start();
+
+}
+
+}
